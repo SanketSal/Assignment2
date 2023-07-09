8791aafdcb55a34b280d68cf08ec58e988dc988f
SanketSal <40586810+SanketSal@users.noreply.github.com>
2023-07-10 02:26:33 +0530

    Create authentication.js

---
+++ src/utils/authentication.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+// Authentication middleware to verify JWT token
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  jwt.verify(token, 'secret-key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token' });
+    }
+
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = {
+  authMiddleware,
+};
